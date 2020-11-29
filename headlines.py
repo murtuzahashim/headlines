b03c81ccdf7bea0e83f30368341c6bb887d2868b
murtuzahashim <54074845+murtuzahashim@users.noreply.github.com>
2020-11-29 12:03:42 -0500

    Update headlines.py

--- headlines.py
+++ headlines.py
@@ -2,6 +2,10 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib
+import urllib3
+from urllib.parse import quote
 
 app = Flask(__name__)
 
@@ -10,15 +14,74 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-@app.route("/")
-def get_news():
-        query = request.args.get("publication")
-        if not query or query.lower() not in RSS_FEEDS:
-                publication = "bbc"
-        else:
-                publication = query.lower()
-        feed = feedparser.parse(RSS_FEEDS[publication])
-        return render_template("home.html",articles=feed['entries'])
 
+
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=bogusid"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=bougusid"
+
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'
+}
+
+@app.route("/")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+       currency_from = DEFAULTS['currency_from']
+       currency_to = request.args.get("currency_to")
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+       currency_to=DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles,weather=weather, currency_from=currency_from, currency_to=currency_to,    rate=rate,currencies=sorted(currencies))
+
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS["publication"]
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
+def get_weather(query):
+    query = quote(query)
+    url = WEATHER_URL.format(query)
+    http = urllib3.PoolManager()
+    response = http.request('GET',url) 
+    data = response.data
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description':parsed['weather'][0]['description'],'temperature':parsed['main']['temp'],'city':parsed['name']}
+    return weather
+
+def get_rate(frm, to):
+        http = urllib3.PoolManager()
+        resp = http.request('GET',CURRENCY_URL)
+        print("Response status: {}".format(resp.status))
+
+      
+        all_currency = resp.data.decode("utf-8") 
+        print(resp.data.decode("utf-8"))
+
+        parsed = json.loads(all_currency).get('rates')
+        frm_rate = parsed.get(frm.upper())
+        to_rate = parsed.get(to.upper())
+        return (to_rate / frm_rate, parsed.keys())
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
+
